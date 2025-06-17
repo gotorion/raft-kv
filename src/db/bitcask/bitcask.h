120337c989e2cf3f2d9bf10eac1e052f62980a01
Junhui Li <junhui.lee@icloud.com>
2025-06-18 00:08:59 +0800

    backup

---
+++ src/db/bitcask/bitcask.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "db.h"
+namespace db {
+class Bitcask final : public DbEngine {
+ public:
+ private:
+};
+}  // namespace db
---
+++ src/main.cpp
@@ -0,0 +1,2 @@
+#include "spdlog/spdlog.h"
+int main(int argc, char** argv) { return 0; }
